8fcce685d64229a68a68298e0f9f73ed0527436f
BrandonY <122407502+by36@users.noreply.github.com>
2023-05-09 13:59:51 -1000

    Update index.js

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //this sends the html file to the web page using the root directory
 //
-app.get("/VolCalc", function(req, res) {
-  res.sendFile(__dirname + "/VolCalculator.html")
+app.get("/HeightCone", function(req, res) {
+  res.sendFile(__dirname + "/HeightCone.html")
 });
 
 // this gets the response from the values in the web page
 app.post("/", function(req, res){
 
-//use n1 for radium and n2 for height
+//use n1 for radius and n2 for volume
   var num1 = Number(req.body.n1);
   var num2 = Number(req.body.n2);
 
 // does the computation of the input variables, as numbers
 //This is a new comment that is gor GitHub
-  var volCylinder = Math.PI * Math.pow(num1, 2) * num2;
+  var heightCone = (3*num2)/(Math.PI * Math.pow(num1, 2));
 
 // sends the results back to the web page as string
-  res.send("The volume of the cylinder is " + volCylinder.toFixed(2));
+  res.send("The height of the cone is " + heightCone.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
 
